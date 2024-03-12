e27d7b216c47aaf6040d834c4d489271b4a0e2d6
Nandan Kumar <nandan@likhitech.com>
2024-03-12 17:26:37 +0530

    header and footer file created

--- lib/screens/landing_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LandingScreen extends StatelessWidget {
-  @override
-  widget build(BuildContext context) {
-    return Scaffold(bold: Body(),);
-  }
-}
