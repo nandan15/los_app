648fe22d60a8dc933600fe84dc291482fd380c52
Srinivas <srinivas@likitech.com>
2024-03-12 16:47:29 +0530

    landing_screen update

---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  @override
+  widget build(BuildContext context) {
+    return Scaffold(bold: Body(),);
+  }
+}
