4c99b471b39fbc0794e0fb9a79642892908d0505
Siddharth S S <siddharthdevtest12345@gmail.com>
2024-03-13 17:46:21 +0530

    >

--- lib/screens/branchSelect/branch_select.dart
+++ lib/screens/branchSelect/branch_select.dart
@@ -41,7 +41,9 @@ class BranchSelectScreen extends StatelessWidget {
           // Select Branch
           Text(
             'Select a Branch:002/Mumbai',
-            style: TextStyle(fontSize: 18,),
+            style: TextStyle(
+              fontSize: 18,
+            ),
           ),
           SizedBox(height: 20),
           // Branch buttons
