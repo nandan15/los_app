80234738c6a4a53f3d10e61503a9ff6b3c6b4025
Srinivas <srinivas@likitech.com>
2024-03-15 16:00:25 +0530

    loan_details fix

--- lib/screens/loan details/loan_details.dart
+++ lib/screens/loan details/loan_details.dart
@@ -17,22 +17,283 @@ class  _LoanDetailsState extends State<LoanDetails> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget> [
           SizedBox(height: 20),
-          Divider(),
-          // fffgg
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children: [
-            //hjghgjgj
-            Container(
-              child: Row(
-                children: [
-                  Text('Profile      LoanDetails   Addresss'),
-                ],),
-            )
-           ],
+          Table(
+            border: TableBorder.all(),
+            children: [TableRow(
+              children: [
+                TableCell(
+                  child: Center(child: Text('Profile',
+                  style: TextStyle(fontSize: 18),
+                  ),
+                  ),
+                ),
+                 TableCell(
+                  child: Center(child: Text('Loan Details',
+                  style: TextStyle(fontSize: 18),
+                  ),
+                  ),
+                ),
+                TableCell(
+                  child: Center(child: Text('Address',
+                  style: TextStyle(fontSize: 18),
+                  ),
+                  ),
+                ),
+              ],
+            ),],
           ),
           SizedBox(height: 20),
-          Divider(),
+          Row(
+            children: <Widget>[
+              Text(
+                'Client loan Cycle Member',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Client ID',
+                  ),
+                )
+              )
+            ],
+          ),
+          //loan id
+                SizedBox(height: 20),
+                // Title Dropdown
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'loan scheme Id',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(width: 20),
+                    DropdownButton<String>(
+                      value: 'loan scheme Id',
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          // Update your value here
+                        });
+                      },
+                      items: <String>['loan scheme Id', 'loan scheme Id', 'loan scheme Id', 'loan scheme Id']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+           SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Expect loan Amount',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Expect loan Amount',
+                  ),
+                )
+              )
+            ],
+          ),
+           SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Expect Term',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Expect Term',
+                  ),
+                )
+              )
+            ],
+          ),
+           SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Expect Repayment Term',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Expect Repayment Term',
+                  ),
+                )
+              )
+            ],
+          ),
+           SizedBox(height: 20),
+                // Title Dropdown
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Template Schedule',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(width: 20),
+                    DropdownButton<String>(
+                      value: 'Template Schedule',
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          // Update your value here
+                        });
+                      },
+                      items: <String>['Template Schedule', 'Template Schedule', ]
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                 SizedBox(height: 20),
+                // Title Dropdown
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Loan Category',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(width: 20),
+                    DropdownButton<String>(
+                      value: 'Loan Category',
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          // Update your value here
+                        });
+                      },
+                      items: <String>['Template Schedule', 'Template Schedule', ]
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Purpose',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Purpose',
+                  ),
+                )
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Disbursement Mode',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Disbursement Mode',
+                  ),
+                )
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Repayment Mode',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Repayment Mode',
+                  ),
+                )
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Repayment Frequency',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Repayment Frequency',
+                  ),
+                )
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Repayment Frequency',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Repayment Frequency',
+                  ),
+                )
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Text(
+                'Repayment Frequency',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Repayment Frequency',
+                  ),
+                )
+              )
+            ],
+          ),
+
+
+
         ],
       )
     );
